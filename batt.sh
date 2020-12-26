026e20f0f7177e17be8c2088bdbecc059b57b257
Ian Winter <ianw@protonmail.com>
2020-12-26 09:28:33 -0800

    Fix batt.sh when acpi finds multiple batteries

--- batt.sh
+++ batt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if OUTPUT="$(acpi -b)" >/dev/null; then
-    if echo $OUTPUT | grep -o "Charging" >/dev/null; then
-        echo -n "C:"
-    elif echo $OUTPUT | grep -o "Discharging" >/dev/null; then
-        echo -n "D:"
+    if echo "$OUTPUT" | grep -o "Charging" >/dev/null; then
+        echo -n " C:"
+    elif echo "$OUTPUT" | grep -o "Discharging" >/dev/null; then
+        echo -n " D:"
     else
         exit 0
     fi
-    echo "$(echo $OUTPUT | grep -o '[0-9]*%') | "
+    echo "$(echo "$OUTPUT" | grep -o -m 1 '[0-9]*%') | "
 fi
